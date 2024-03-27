528ab4c722616608e67f2a9e91137f1267c30dbb
Gabriella Wishart <160987958+GabbyWishart@users.noreply.github.com>
2024-03-26 20:15:10 -0700

    Update script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+  function myMenu() {
+    document.getElementById('mymenu').classList.toggle( 'open');
+    document.getElementById('menuButton').classList.toggle('close');
+
+  }
+
+  function closemenu() {
+    document.getElementById('mymenu').classList.remove( 'open');
+    document.getElementById('menuButton').classList.remove('close');
+
+  }
+
