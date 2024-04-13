14a8c72388c98695090aae5b495131c5e9e02d60
Gabriella Wishart <160987958+GabbyWishart@users.noreply.github.com>
2024-04-12 17:39:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 
-  function myMenu() {
+function darkmode() {
+  document.getElementsByClassName('main_nav')[0].classList.toggle('main_nav_dark_mode');
+  document.getElementsByClassName('main_header')[0].classList.toggle('main_header_dark_mode');
+  document.getElementsByClassName('about')[0].classList.toggle('about_dark_mode');
+  document.getElementsByClassName('about_me_image')[0].classList.toggle('about_me_image_dark_mode');
+  document.getElementById('body')[0].classList.toggle('body_dark_mode');
+
+
+
+
+
+}
+
+
+ function myMenu() {
     document.getElementById('mymenu').classList.toggle( 'open');
     document.getElementById('menuButton').classList.toggle('close');
 
@@ -11,8 +25,4 @@
 
   }
 
-  function darkmode() {
-    document.getElementsByClassName('body')[0].classList.toggle('body-dark-mode');
-
-  }
-
+  
